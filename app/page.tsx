d9523e628d936d591b4b7e34e5a763c7c9a408b5
Ömer Faruk Aksak <oemerfaruk.aksak@kuleuven.be>
2025-03-25 13:24:21 +0100

    added login page html

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,11 @@
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 export default function Home() {
-  const { data, status } = useSession();
 
-  console.log("Session", data);  // Log the session
-  console.log("Status", status);
   return (
     
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        
+        test
     </div>
   );
 }
